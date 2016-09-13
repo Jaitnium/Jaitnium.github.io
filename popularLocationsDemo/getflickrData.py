53c7738c026d688c80df739ffbb0667d4f255880
Jaitnium <matthewaantosiak@gmail.com>
2016-09-13 11:35:46 -0700

    Trying to sync to github

--- popularLocationsDemo/getflickrData.py
+++ popularLocationsDemo/getflickrData.py
@@ -4,6 +4,7 @@ import sys
 import datetime
 import os
 import textwrap
+import numpy as np
 
 #"Month" and "Year" query flickr
 #mintakenDate and maxtakenDate are created using the given month and year
@@ -290,7 +291,7 @@ def verifyArguments(flag):
             return;
     #Check argument for "update index.json" usage
     if(flag == '-ui'):
-        return;      
+        return;     
     #Any other arguments will cause the check to fail and exit
     else:
         printAllUsage()
@@ -314,10 +315,10 @@ def createMonthJSON(placeToFind, month, year):
     
 
     #Check for year subdirectory
-    path = "./mapData/" + year
+    #path = "./mapData/" + year
     #Create if it doesn't exist
-    if not os.path.exists(path):
-        os.makedirs(path)
+    #if not os.path.exists(path):
+        #os.makedirs(path)
 
     #Parse the name of the place from the returned filename
     #and create a folder with that name if it doesn't exist
@@ -349,6 +350,233 @@ def createIndexJSON(path):
     else:
         d['type'] = "file"
     return d
+
+
+
+#FUTURE IMPLEMENTATION
+#Possible incoporate into queryFlickrData after data is retreived
+#Given a dictionary in the same format as the JSON filescreated in the queryFlickrData function
+#Return a new dictionary without outliers
+def removeOutliersDictionary(data):
+    return data
+
+def getFlickrPlace(placeToFind):
+    #Needed to access the API
+    api_key = '082ca8bbb880bc514637527a4bfbe557'
+    api_secret = 'c60206df6e5c1df1'
+
+    #Create flickr handlers
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, format="parsed-json")
+    flickrJSON = flickrapi.FlickrAPI(api_key, api_secret, format="json")
+
+    #Fetch the location using the given string
+    places = flickr.places.find(query=placeToFind)   
+
+    #If nothing was returned, signal error and exit
+    if(len(places['places']['place'])) == 0:
+        print("ERROR: Invalid 'placeToFind' string. flickr.places.find returned with an empty array. Exiting...")
+        sys.exit(0)
+    placeFound = places['places']['place'][0]
+
+    actualLat = placeFound['latitude']
+    actualLng = placeFound['longitude']
+
+    print("Actual lat + lng: " + actualLat + " " + actualLng)
+
+    return (float(actualLat), float(actualLng))
+
+def renameExistingFile(path, outputName, i):
+
+    #Split into name + .json
+    file = outputName.split(".")
+
+    #Try to rename the old JSON file
+    try:
+    #print("Calling:")
+    #print(path + "/" + outputName + "\nto\n" + path + "/" + file[0] + "_" + str(i) + "." + file[1])
+        os.rename(path + "/" + outputName, path + "/" + file[0] + "_" + str(i) + "." + file[1])
+    except OSError:
+        #Try to rename with a higher index
+        renameExistingFile(path, outputName, i + 1)
+
+#Removes the outliers from the given JSON file
+def removeOutliers(path):
+    print("Going to remove outliers from file " + path)
+    with open(path) as data_file:
+        #Determines removal threshold
+        #Larger means less will be removed (if any)
+        quartileMultipler = 2.0
+
+        data = json.load(data_file)
+        print("Total entries: " + str(len(data['photos']['photo'])))
+        #for i in range(0, len(data.photos.photo))
+        #Calculate the standard deviation for all the geolocations
+
+        #Where the data will be stored
+        #List of dictionaries will be added will be in the form: {lat, lng, dataIndex}
+        #Any remaining data points after calculations will be returned
+        retDictList = [];
+
+        # First: Calculate the mean
+        avgLat = 0; avgLng = 0;
+        for i in range(0, len(data['photos']['photo'])):
+            lat = float(data['photos']['photo'][i]['latitude'])
+            lng = float(data['photos']['photo'][i]['longitude'])
+            avgLat += lat
+            avgLng += lng
+
+            #Create an entry and add it to the array
+            dictEntry = {'lat' : lat, 'lng' : lng, 'index' : i}
+            retDictList.append(dictEntry)
+
+        avgLat /= len(data['photos']['photo'])
+        avgLng /= len(data['photos']['photo'])
+        print("Avg lat + long: " + str(avgLat) + " " + str(avgLng))
+
+        #Split path
+        temp = path.split("/")
+        #Get filename
+        temp = temp[len(temp) - 1]
+        #Get place from filename
+        place = temp.split("_")[0]
+        print(place)
+
+        actualLatLng = getFlickrPlace(place)
+
+        #Second : Calculate the distance between each point and the avgLat and avgLng
+        for i in range(0, len(retDictList)):
+            dist = calcDistance(retDictList[i]['lat'], retDictList[i]['lng'], actualLatLng[0], actualLatLng[1])
+            #Add calculated dist to entry
+            retDictList[i]['dist'] = dist
+
+        #Sort retDictList by their dist value
+        retDictList = sorted(retDictList, key=lambda k: k['dist'])
+
+        #Calculate median
+        #Array to hold dist values when calculating median
+        tempArray = []
+        for i in range(0, len(retDictList)):
+            tempArray.append(retDictList[i]['dist'])
+
+        quartile2 = median(tempArray)
+
+        #Now, need to calculate Quartile1 and Quartile3
+        #Create two arrays, one with all dist values lower than the medianVal
+        #and another will all values greater than the median value
+        lowerArray = []; higherArray=[];
+        for i in range(0, len(tempArray)):
+            #If lower, add to lower array
+            if(tempArray[i] < quartile2):
+                lowerArray.append(tempArray[i])
+            else:
+                higherArray.append(tempArray[i])
+        quartile1 = median(lowerArray)
+        print("quartile1: " + str(quartile1))
+        print("quartile2: " + str(quartile2))
+        quartile3 = median(higherArray)
+        print("quartile3: " + str(quartile3))
+        IQR = quartile3 - quartile1
+        print("IQR: " + str(IQR))
+        print("Quartile multipler: " + str(quartileMultipler))
+        print("Removal threshold: " + str(quartile1 - quartileMultipler * IQR) + " " + str(quartile3 + quartileMultipler * IQR))
+
+        #Now we have quartile1, 2, 3, and the IQR, which is enough to calculate outliers
+        #Gowe're going to save the indicies of the data entries to remove
+        amountRemoved = 0
+        i = 0
+        indiciesToPop = []
+        for i in range(0, len(retDictList)):
+            #If val <= Q1 - quartileMultipler *IQR
+            if(retDictList[i]['dist'] <= quartile1 - quartileMultipler * IQR):
+                #This is the index in the data dictionary to remove
+                indiciesToPop.append(retDictList[i]['index'])
+                amountRemoved += 1
+            #Else If val >= Q3 + quartileMultipler * IQR
+            elif(retDictList[i]['dist'] >= quartile3 + quartileMultipler * IQR):
+                #This is the index in the data dictionary to remove
+                indiciesToPop.append(retDictList[i]['index'])
+                amountRemoved += 1
+
+        print("Going to remove " + str(amountRemoved) + " entries..")
+
+        #Now retDictList has its outliers removed
+        #We can now get an array of the retDictList indicies to know which values to keep
+        indiciesToPop = sorted(indiciesToPop)
+        #for i in range(0, len(indiciesToPop)):
+        #    print(indiciesToPop[i])
+
+        #Change the "data" dictionary to reflect the removed elements
+        #Traverse the the array backwards to bypass the problem of the array constantly shifting after popping indices
+        #Start at the last index
+        i = len(indiciesToPop) - 1;
+        while i >= 0:
+            data['photos']['photo'].pop(indiciesToPop[i])
+            i -= 1
+        print("new length: " + str(len(data['photos']['photo'])))
+
+        #We're done! Data is updated and doesn't have any outliers
+        #Write to a file and we're good as gravy
+
+        temp = path.split("/")
+        outputName = temp[len(temp) - 1]
+        print("Going to create output file with new data into: " + outputName)
+        #Dump into a JSON file named the same as this one
+        with open(outputName, 'w') as outfile:
+            json.dump(data, outfile, indent=4, sort_keys=True, separators=(',', ':'))
+
+        print("Moving newly created file into the appropriate mapData directory and placing the old JSON file in this directory...")
+
+        #Testing directory creation
+        #Going to move
+        #path = "./mapData"
+        #Create if it doesn't exist
+        #if not os.path.exists(path):
+            #os.makedirs(path)
+        #Check for year subdirectory
+        #path = "./mapData/" + year
+        #Create if it doesn't exist
+         #if not os.path.exists(path):
+            #os.makedirs(path)
+
+        #Parse the name of the place from the returned filename
+        #and create a folder with that name if it doesn't exist
+        #placeName = fileName.split("_")[0]
+
+        #Check for month subdirectory
+        #path = "./mapData/" + placeName + "/" + year + "/" + month
+        #Create if it doesn't exist
+        #if not os.path.exists(path):
+            #os.makedirs(path)
+
+        #os.rename("mapData/San Francisco/2015/October/super test2.json", "mapData/San Francisco/2015/October/super test3.json")
+        #print("mapData/San Francisco/2015/October/super test1.json")
+        #print("mapData/San Francisco/2015/October/super test2.json")
+        #os.rename("mapData/San Francisco/2015/October/San Francisco.json", "mapData/San Francisco/2015/October/San Francisco.json")
+        #sys.exit()
+
+    print("fileName: " + outputName)
+
+    args = path.split("/")
+
+    newPath = "mapData/" + args[1] + "/" + args[2] + "/" + args[3]
+    #Create if it doesn't exist
+    if not os.path.exists(newPath):
+        print("Path doesn't exist, creating new path!")
+        os.makedirs(newPath)
+
+    #Call recursive helper to rename existing old file
+    renameExistingFile(newPath, args[4], 1)
+
+    #Move the newly created file to the correct directory
+    try:
+        os.rename(outputName, newPath + "/" + outputName)
+    except OSError:
+        #If the file exists, delete it and move the new file there
+        print("File Exists: BUG, returning without moving the new file..")
+        return
+        os.remove(path + "/" + outputName)
+        os.rename(outputName, path + "/" + outputName)
+
 
 #MAIN START
 #outfile is the name of the json file that contains the mapData structure
@@ -415,4 +643,4 @@ if(sys.argv[1] == '-ui'):
     with open(outfile, 'w') as outfile:
        json.dump(createIndexJSON('mapData'), outfile, indent=4, sort_keys=True, separators=(',', ':'))
 
-    print("index.json updated")    
+    print("index.json updated") 
--- popularLocationsDemo/ghostTest.py
+++ popularLocationsDemo/ghostTest.py
@@ -7,6 +7,7 @@
 import ghost
 import sys
 import json
+from googleplaces import GooglePlaces, types, lang
 
 #Given the raw output from the webpage
 #Return a dictionary of the lat/long data
@@ -38,6 +39,7 @@ def rawToJSON(raw, year, month, place, zoom):
 	HSDict['month'] = month
 	HSDict['place'] = place
 	HSDict['zoom'] = zoom
+	HSDict['entries'] = len(HSArray)
 	HSDict['array'] = HSArray
 
 	print(HSDict)
@@ -54,9 +56,9 @@ def getRawData(year, month, place, zoom):
         #print(result)
         #sys.exit()
         page, extra_resources = session.open("https://jaitnium.github.io/popularLocationsDemo/flickrTest.html")
-        result, resources = session.evaluate("getHotSpotJSON('2015', 'January', 'Sanfrancisco', 17)")
+        result, resources = session.evaluate("getHotSpotJSON('2015', 'January', 'San Francisco', 15)")
         print(result)
-        result, resources = session.wait_for_alert()
+        result, resources = session.wait_for_alert(100)
         #print(result)
         return result
         #results, resources = session.wait_for_text("Total")
@@ -68,9 +70,54 @@ def getRawData(year, month, place, zoom):
 #MAIN
 #python ghostTest.py year month placeString zoom
 
-if(sys.argv[1] == "-usage" or sys.argv[1] == "-u"):
-	print("Usage: python ghostTest.py [year] [month] [place] [zoom]")
-	sys.exit()
+#if(sys.argv[1] == "-usage" or sys.argv[1] == "-u"):
+#	print("Usage: python ghostTest.py [year] [month] [place] [zoom]")
+#	sys.exit()
+
+
+
+def build_URL(lat, lng, radius):
+    base_url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+    location = 'location=' + lat + "," + lng
+    radius = 'radius=' + radius
+    key_string = '&key='+'082ca8bbb880bc514637527a4bfbe557'
+    url = base_url+location+radius+key_string
+    return url
+
+#url = build_URL("37.80864212711866", "-122.47142523728814", "10")
+#print(url)
+#response = urlopen(url)
+#print(response)
+
+YOUR_API_KEY = 'AIzaSyCsWHtQAvGirxFXX6Ccmg6lkBj8Su80law'
+
+google_places = GooglePlaces(YOUR_API_KEY)
+
+query_result = google_places.nearby_search(lat_lng={'lat' : 37.81056165132787, 'lng' : -122.4105678498745}, radius=10)
+
+
+for place in query_result.places:
+    # Returned places from a query are place summaries.
+    print(place.name)
+    print("\n")
+    print(place.geo_location)
+    print("\n")
+    print(place.place_id)
+    print("\n")
+
+    # The following method has to make a further API call.
+    place.get_details()
+    # Referencing any of the attributes below, prior to making a call to
+    # get_details() will raise a googleplaces.GooglePlacesAttributeError.
+    print(place.details) # A dict matching the JSON response from Google.
+    print(place.local_phone_number)
+    print(place.international_phone_number)
+    print(place.website)
+    print(place.url)
+
+    # Getting place photos
+
+sys.exit()
 
 year = sys.argv[1]
 month = sys.argv[2]
@@ -88,7 +135,7 @@ print("Retrieved data.. converting to dictionary")
 
 HSDict = rawToJSON(rawHSData, year, month, place, zoom)
 
-outputName = place + "_" + month + "_" + year + "_" + "HS.json"
+outputName = "HS" + "_" + place + "_" + month + "_" + year + ".json"
 
 print("Writing data to " + outputName + " ...")
 
